b663cdd0f66dcfb4ca4155bd9ca4cfda8d147050
Negrut Cosmin <cosminnegrut776@gmail.com>
2025-03-19 02:30:59 +0200

    render the list every time we add a task

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -4,18 +4,25 @@ const todoList=[
   'play football',
   'admire Ronaldo'
 ]
-let todoListHtml='';
+renderToDoList()
 
-for (let i=0;i< todoList.length; i++){
-  const todo= todoList[i]
-  const html=`<p>${todo}</p>`
-  todoListHtml+=html
 
+function renderToDoList(){
+
+
+  let todoListHtml='';
+
+  for (let i=0;i< todoList.length; i++){
+    const todo= todoList[i]
+    const html=`<p>${todo}</p>`
+    todoListHtml+=html
+
+  }
+
+  console.log(todoListHtml);
+
+  document.querySelector('.js-todo-list').innerHTML=todoListHtml
 }
-
-console.log(todoListHtml);
-
-document.querySelector('.js-todo-list').innerHTML=todoListHtml
 
 function addTodo(){
  const inputElement= document.querySelector('.js-name-input')
@@ -23,5 +30,7 @@ function addTodo(){
  todoList.push(name)
  console.log(todoList);
  inputElement.value=''
+
+ renderToDoList()
 }
 
